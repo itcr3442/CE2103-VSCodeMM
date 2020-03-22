278c4460d2cad90b98723dc10f50ad4701eb6e0a
Alejandro Soto <alejandro@34project.org>
2020-03-21 19:19:16 -0600

    Support remote managers on library initialization

--- include/ce2103/mm/init.hpp
+++ include/ce2103/mm/init.hpp
@@ -5,6 +5,9 @@ namespace ce2103::mm
 {
 	//! Initializes the library for local operation, ignoring network hints.
 	void initialize_local();
+
+	//! Initializes the library for local and (according to hints) remote operation.
+	void initialize();
 }
 
 #endif
--- src/misc.cpp
+++ src/misc.cpp
@@ -4,8 +4,11 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "ce2103/network.hpp"
+
 #include "ce2103/mm/gc.hpp"
 #include "ce2103/mm/init.hpp"
+#include "ce2103/mm/client.hpp"
 
 namespace
 {
@@ -19,6 +22,45 @@ namespace
 		// This starts the GC thread.
 		default_manager = &ce2103::mm::garbage_collector::get_instance();
 	}
+
+	//! Unlocked body of initialize().
+	void do_initialize()
+	{
+		using ce2103::mm::remote_manager, ce2103::socket, ce2103::ip_endpoint;
+
+		bool connected = false;
+		if(const char* endpoint_string = std::getenv("MM_SERVER"); endpoint_string != nullptr)
+		{
+			socket client_socket;
+			if(auto endpoint = ip_endpoint::try_from(endpoint_string); !endpoint)
+			{
+				std::cerr << "=== Invalid endpoint '" << endpoint_string << "' ===\n";
+			} else if(const char* key = std::getenv("MM_PSK"); key == nullptr)
+			{
+				std::cerr << "=== MM_SERVER is set but not MM_PSK ===\n";
+			} else if(!client_socket.connect(*endpoint))
+			{
+				std::cerr << "=== Connection to server failed ===\n";
+			} else if(!remote_manager::initialize(std::move(client_socket), key))
+			{
+				std::cerr << "=== Handshake failed (wrong MM_PSK?) ===\n";
+			} else
+			{
+				connected = true;
+				default_manager = &remote_manager::get_instance();
+			}
+
+			if(!connected)
+			{
+				std::cerr << "=== Remote setup failed, falling back to local services ===\n";
+			}
+		}
+
+		if(!connected)
+		{
+			do_initialize_local();
+		}
+	}
 }
 
 namespace ce2103::mm
@@ -26,6 +68,11 @@ namespace ce2103::mm
 	void initialize_local()
 	{
 		std::call_once(initialization_flag, do_initialize_local);
+	}
+
+	void initialize()
+	{
+		std::call_once(initialization_flag, do_initialize);
 	}
 
 	memory_manager& memory_manager::get_default() noexcept
