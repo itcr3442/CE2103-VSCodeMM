3f1303b4b6a73fcca0d1527a4aacb3fdd8131c4b
Alejandro Soto <alejandro@34project.org>
2020-06-10 17:02:57 -0600

    Clean-up the extension

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,16 +1,55 @@
-import * as vscode from 'vscode';
-import {ExtensionFunctions} from './extensionFunctions';
+import * as fs from "fs-extra";
+import * as path from "path";
+import * as vscode from "vscode";
+
+class ServerList implements vscode.TreeDataProvider<Server> {
+  constructor() {}
+
+  getTreeItem(element: Server): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: Server): Thenable<Server[]> {
+    return Promise.resolve([]);
+  }
+}
+
+class Server extends vscode.TreeItem {
+  private workspace;
+  constructor(
+    public readonly label: string,
+    private address: string,
+    private port: number,
+    private secret: string,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState
+  ) {
+    super(label, collapsibleState);
+  }
+
+  iconPath = {
+    light: path.join(
+      __filename,
+      "..",
+      "..",
+      "resources",
+      "light",
+      "dependency.svg"
+    ),
+    dark: path.join(
+      __filename,
+      "..",
+      "..",
+      "resources",
+      "dark",
+      "dependency.svg"
+    ),
+  };
+}
 
 export function activate(cobtext: vscode.ExtensionContext) {
-    const extensionFunctions = new ExtensionFunctions(vscode.workspace.rootPath);
-    vscode.window.registerTreeDataProvider('Servers', extensionFunctions);
-    vscode.commands.registerCommand('Servers.refreshEntry', () =>
-        extensionFunctions.refresh()
-    );
-    vscode.commands.registerCommand('Servers.startExtension', () =>
-        extensionFunctions.startExtension()
-    );
-    vscode.commands.registerCommand('Servers.addServer', () => 
-        extensionFunctions.addServer()
-    );
+  vscode.window.registerTreeDataProvider("vscodemm.servers", new ServerList());
+
+  vscode.commands.registerCommand("vscodemm.setup", () =>
+    fs.copySync("vscodemm/", path.join(vscode.workspace.rootPath, "vscodemm/"))
+  );
 }
